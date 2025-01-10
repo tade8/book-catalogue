dfdb225f4240c536c5ad23d182ba6dc89843887c
Tade Emmanuel <tadeemmanuel08@gmail.com>
2025-01-10 08:53:10 +0100

    initial project structure

---
+++ managementservice/src/test/java/com/org/managementservice/ManagementserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.managementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
